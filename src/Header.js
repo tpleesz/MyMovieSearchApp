d459e44f621166d2a512235391115b2abdbacac6
tpleesz <78024107+tpleesz@users.noreply.github.com>
2021-01-26 15:39:12 +0100

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render(){
+        return ( 
+          <nav className = "relative mx-auto bg-indigo-700 max-w-7xl py-4 px-4">
+             <div class="container mx-auto">
+                  <h1 class="text-white text-center text-3xl pb-4"> Movie Search </h1>
+              </div>
+          </nav>
+    
+        )
+    }
+}
+
+export default Header
